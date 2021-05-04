ecdc869c8608315fbea9adc649c809126873aab6
kpilszak <klaudia.pilszak@gmail.com>
2021-05-04 17:00:36 +0200

    Create project

---
+++ src/main/java/com/kpilszak/ecommercedashboard/EcommerceDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.kpilszak.ecommercedashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDashboardApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceDashboardApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/kpilszak/ecommercedashboard/EcommerceDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpilszak.ecommercedashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceDashboardApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
